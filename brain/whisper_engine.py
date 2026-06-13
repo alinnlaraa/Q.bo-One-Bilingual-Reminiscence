c0c89412d22eb311066d7e235184fafb881b8298
alinnlaraa <113515410+alinnlaraa@users.noreply.github.com>
2026-06-13 16:38:17 +0200

    Update whisper_engine.py

--- brain/whisper_engine.py
+++ brain/whisper_engine.py
@@ -26,12 +26,6 @@ class Transcriber:
         return text
         
         
-        #print(f"[Whisper] Transcribing {audio_path}...")
-        #segments, info = self.model.transcribe(audio_path)
-        #text=" ".join(segment.text for segment in segments)
-        #print("[STT Result]:", text)
-        #return text
-
 
 
 
